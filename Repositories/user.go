36157e0988b38f8a5a0a21d32717c9e40213f280
darkd <esteban.herrera.1126@gmail.com>
2020-08-09 19:18:19 -0500

    Refactor connection database from user repository

--- Repositories/user.go
+++ Repositories/user.go
@@ -8,18 +8,27 @@ import (
 	"github.com/d97arkslayer/twitter-go/Utils"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
 	"time"
 )
 
+/**
+ * setupConnection
+ * Use this to do operations over collection Users
+ */
+func setupConnection(db string, coll string) (*mongo.Collection, context.Context, context.CancelFunc){
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	database := Database.MongoConnection.Database(db)
+	collection := database.Collection(coll)
+	return collection, ctx, cancel
+}
 /**
  * InsertUser
  * This function inserts a new user in mongo
  */
 func InsertUser(user Models.User)(string, bool, error){
-	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	collection, ctx, cancel := setupConnection("twitter-go", "users")
 	defer cancel()
-	database := Database.MongoConnection.Database("twitter-go")
-	collection := database.Collection("users")
 	user.Password, _ = Utils.EncryptPassword(user.Password)
 	result, err := collection.InsertOne(ctx, user)
 	if err != nil {
@@ -34,10 +43,8 @@ func InsertUser(user Models.User)(string, bool, error){
  * This function search an user with the provided email
  */
 func ExistUser(email string)(Models.User, bool, string){
-	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	collection, ctx, cancel := setupConnection("twitter-go", "users")
 	defer cancel()
-	database := Database.MongoConnection.Database("twitter-go")
-	collection := database.Collection("users")
 	condition := bson.M{"email":email}
 	var result Models.User
 	err := collection.FindOne(ctx, condition).Decode(&result);
@@ -69,10 +76,8 @@ func Login(email string, password string) (Models.User, bool)  {
  * Use to find a user profile
  */
 func ShowProfile(Id string)(Models.User, error){
-	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	collection, ctx, cancel := setupConnection("twitter-go", "users")
 	defer cancel()
-	database := Database.MongoConnection.Database("twitter-go")
-	collection := database.Collection("users")
 	var profile Models.User
 	objId, _ := primitive.ObjectIDFromHex(Id)
 	condition := bson.M{ "_id":objId }
@@ -84,3 +89,12 @@ func ShowProfile(Id string)(Models.User, error){
 	}
 	return profile, nil
 }
+/*
+/**
+ * UpdateUser
+ * Use this to update a user record
+ */
+/*
+func UpdateUser(u Models.User, Id, string) (bool, error){
+
+}*/
